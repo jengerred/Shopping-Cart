7ffcfe73ffd20a2b53ef9f206aacf077ff48650f
jengerred <56004257+jengerred@users.noreply.github.com>
2023-02-01 21:45:30 -0500

    initial commit

---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+        id: '1',
+        name:'Shoes',
+        price: 400,
+        image: 'https://picsum.photos/id/21/2400/1600',
+        },
+        {
+        id: '2',
+        name:'Macbook',
+        price: 1400,
+        image: 'https://picsum.photos/id/0/2400/1600',
+        },
+        {
+        id: '3',
+        name:'Camera',
+        price: 500,
+        image: 'https://picsum.photos/id/454/2400/1600',
+        },
+    
+       /* {
+            id: '4',
+            name:'Fruit',
+            price: 3,
+            image: 'https://loremflickr.com/320/240/pastry?random=2',
+            },
+            {
+                id: '5',
+                name:'Fruit',
+                price: 2,
+                image: 'https://loremflickr.com/320/240/pastries?random=3',
+                },
+                {
+                    id: '6',
+                    name:'Fruit',
+                    price: 5,
+                    image: 'https://loremflickr.com/320/240/pastries?random=4',
+                    }*/
+    ]
+};
+export default data;
